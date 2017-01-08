623957896dac59bb4388e0ec049c7756f1011519
Weerayut Hongsa <kusumoto.com@gmail.com>
2017-01-09 02:20:56 +0700

    Create run-scanner.sh

---
+++ run-scanner.sh
@@ -0,0 +1,10 @@
+wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip-O ~/scanner.zip
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
+  cat << EOF > sonar-project.properties
+  sonar.host.url=https://sonar.kusumotolab.com
+  sonar.projectKey=com.kusumotolab.psuautoreg:master
+  sonar.projectName=psu-sis-autoregister
+  sonar.projectVersion=1.0
+  sonar.sources=.
+EOF
